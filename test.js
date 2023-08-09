3e2f2c4a02dda7c3ef6d9722a72c00273a33efdd
rayhan3030 <rayhanddd@gmail.com>
2023-08-09 06:49:40 +0600

    first commit yeeeeeeeeeeee

---
+++ test.js
@@ -0,0 +1,47 @@
+
+// KG to Ton converter js starts here =========================================================
+function weightConverter(nunvalue){
+    document.getElementById("ton_output").innerHTML = nunvalue/907;
+}
+
+// Ton to KG converter js starts here =========================================================
+function ton2kgconverter(nunvalue){
+    document.getElementById("ton2kg").innerHTML = nunvalue*907;
+}
+
+// Miligram to Kilogram converter js starts here =========================================================
+function mg2kg(nunvalue){
+    document.getElementById("mg2kg").innerHTML = nunvalue/1000;
+}
+
+// Petrol Price converter js starts here =========================================================
+function petrol(nunvalue){
+    document.getElementById("petrol").innerHTML = nunvalue*130;
+}
+
+// Disel Price converter js starts here =========================================================
+function disel(nunvalue){
+    document.getElementById("disel").innerHTML = nunvalue*105;
+}
+
+// Octain Price converter js starts here =========================================================
+function octain(nunvalue){
+    document.getElementById("octain").innerHTML = nunvalue*135;
+}
+
+// Inch to Feet converter js starts here =========================================================
+function inch2feet(nunvalue){
+    document.getElementById("inch2feet").innerHTML = nunvalue/12;
+}
+
+
+// Feet to Meter converter js starts here =========================================================
+function feet2meter(nunvalue){
+    document.getElementById("feet2meter").innerHTML = nunvalue*0.3048;
+}
+
+// Meter to Kilometer converter js starts here =========================================================
+function meter2kilometer(nunvalue){
+    document.getElementById("meter2kilometer").innerHTML = nunvalue/1000;
+}
+
